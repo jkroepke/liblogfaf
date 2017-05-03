d085db79e903f58bb1193f547d6ed3b9208198a6
Jan-Otto Kröpke <jkroepke@users.noreply.github.com>
2017-05-03 13:27:06 +0200

    Use stdout instead stderr

--- src/liblogfaf.c
+++ src/liblogfaf.c
@@ -68,117 +68,13 @@ static void set_defaults(SharedData *sd) {
     sd->syslog_facility = 0;
 }
 
-static void init_progname(SharedData *sd) {
-#if defined(__APPLE__)
-    sscanf(*_NSGetProgname(), "%1023s", sd->progname);
-#elif defined(__FreeBSD__)
-    sscanf(getprogname(), "%1023s", sd->progname);
-#else
-    FILE* cmdline = fopen("/proc/self/cmdline", "rb");
-    if (cmdline) {
-        fscanf(cmdline, "%1023s", sd->progname);
-        fclose(cmdline);
-    }
-#endif
-}
-
-static void init_hostname(SharedData *sd) {
-    if (gethostname(sd->hostname, sizeof(sd->hostname)) != 0) {
-        perror("liblogfaf: gethostname() error");
-        exit(1);
-    }
-    // We don't really need FQDN, so we truncate the hostname up until
-    // the first dot.
-    char *dot_ptr = strchr(sd->hostname, '.');
-    if (dot_ptr != NULL) {
-        *dot_ptr = '\0';
-    }
-}
-
-static void init_connection(SharedData *sd) {
-    char *server_hostname;
-    server_hostname = getenv("LIBLOGFAF_SERVER");
-    if (server_hostname == NULL)
-        server_hostname = "localhost";
-
-    char *server_port;
-    server_port = getenv("LIBLOGFAF_PORT");
-    if (server_port == NULL)
-        server_port = "514";
-
-    char *bind_ip;
-    bind_ip = getenv("LIBLOGFAF_BIND_IP");
-
-    int gai_error;
-    struct addrinfo hints;
-
-    memset(&hints, 0, sizeof(struct addrinfo));
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_DGRAM;
-    hints.ai_flags = 0;
-    hints.ai_protocol = 0;
-
-    gai_error = getaddrinfo(server_hostname, server_port,
-                            &hints, &sd->serveraddr);
-    if (gai_error != 0) {
-        fprintf(stderr, "liblogfaf: getaddrinfo() failed: %s\n",
-                gai_strerror(gai_error));
-        exit(1);
-    }
-
-    if ((sd->sockfd = socket(sd->serveraddr->ai_family,
-                             sd->serveraddr->ai_socktype,
-                             sd->serveraddr->ai_protocol)) < 0) {
-        perror("liblogfaf: cannot create socket");
-        exit(1);
-    }
-
-    if (bind_ip) {
-        gai_error = getaddrinfo(bind_ip, 0,
-                                &hints, &sd->bind_ip);
-        if (gai_error != 0) {
-            fprintf(stderr,
-                    "liblogfaf: getaddrinfo() failed for "
-                    "LIBLOGFAF_BIND_IP: %s\n",
-                    gai_strerror(gai_error));
-            exit(1);
-        }
-        if (bind(sd->sockfd, sd->bind_ip->ai_addr, sd->bind_ip->ai_addrlen)) {
-            perror("liblogfaf: bind() failed");
-            exit(1);
-        }
-    }
-}
-
 static void logmessage(SharedData *sd, int priority, const char *message) {
     DBG(("liblogfaf: logmessage(%d, %s)\n", priority, message));
-    time_t ts;
-    struct tm time_tm;
-    char msg[MAX_MESSAGE_LEN];
-    ts = time(NULL);
-    localtime_r(&ts, &time_tm);
-
-    snprintf(msg, MAX_MESSAGE_LEN, "<%u>%s %2d %02d:%02d:%02d %s %s: %s",
-             priority + sd->syslog_facility * 8,
-             months[time_tm.tm_mon], time_tm.tm_mday,
-             time_tm.tm_hour, time_tm.tm_min, time_tm.tm_sec,
-             (char *)&sd->hostname, sd->syslog_tag, message);
-
-    // We want fire-and-forget, so lack of error checking here is intentional
-    sendto(sd->sockfd, msg, strlen(msg), 0,
-           sd->serveraddr->ai_addr, sd->serveraddr->ai_addrlen);
+    printf("%s", message);
 }
 
 __attribute__((constructor)) static void _liblogfaf_init(void) {
     DBG(("liblogfaf: init()\n"));
-    init_progname(&shared_data);
-    init_hostname(&shared_data);
-    init_connection(&shared_data);
-    if (pthread_mutex_init(&shared_data.lock, NULL) != 0) {
-        fprintf(stderr, "liblogfaf: pthread_mutex_init() failed\n");
-        exit(1);
-    }
-    set_defaults(&shared_data);
 }
 
 __attribute__((destructor)) static void _liblogfaf_fini(void) {
@@ -187,36 +83,15 @@ __attribute__((destructor)) static void _liblogfaf_fini(void) {
         fprintf(stderr, "liblogfaf: pthread_mutex_destroy() failed\n");
         exit(1);
     }
-    freeaddrinfo(shared_data.serveraddr);
 }
 
 void openlog(const char *ident, int option, int facility) {
     DBG(("liblogfaf: openlog(%s, %d, %d)\n", ident, option, facility));
-    if (pthread_mutex_lock(&shared_data.lock) != 0) {
-        fprintf(stderr, "liblogfaf: pthread_mutex_lock() failed\n");
-        exit(1);
-    }
-    shared_data.syslog_facility = facility;
-    if (ident)
-        shared_data.syslog_tag = ident;
-    if (pthread_mutex_unlock(&shared_data.lock) != 0) {
-        fprintf(stderr, "liblogfaf: pthread_mutex_unlock() failed\n");
-        exit(1);
-    }
     // making use of the `option` parameter can be added here if you need it
 }
 
 void closelog(void) {
     DBG(("liblogfaf: closelog()\n"));
-    if (pthread_mutex_lock(&shared_data.lock) != 0) {
-        fprintf(stderr, "liblogfaf: pthread_mutex_lock() failed\n");
-        exit(1);
-    }
-    set_defaults(&shared_data);
-    if (pthread_mutex_unlock(&shared_data.lock) != 0) {
-        fprintf(stderr, "liblogfaf: pthread_mutex_unlock() failed\n");
-        exit(1);
-    }
 }
 
 void __syslog_chk(int priority, int flag, const char *format, ...) {
